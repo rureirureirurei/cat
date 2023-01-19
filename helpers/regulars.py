0fa472fa219e0d53671400716e7d4890d769d30b
Denys Zinoviev <reiisbetterthanasuka@gmail.com>
2023-01-19 17:34:14 +0100

    Replaced tuples with dict

--- helpers/regulars.py
+++ helpers/regulars.py
@@ -20,15 +20,12 @@ def _float(seq):
     return []
 
 def _fixed(seq):
-    #TODO
     return []
 
 def _money(seq):
-    #TODO
     return []
 
 def _percentage(seq):
-    #TODO
     return []
 
 
@@ -60,9 +57,9 @@ def regulars(batch, header):
   
     for reg in _regulars:
         # Check header
-        for hdr in reg[0]['header']:
+        for hdr in reg['header']:
             if hdr in header:
-                tags.add(reg[1])
+                for t in reg['tags']: tags.add(t)
         
         # Check regexes, count average
         res = 0
@@ -71,24 +68,26 @@ def regulars(batch, header):
             if len(str(s)) > max_len:
                 continue
             match = False
-            for regx in reg[0]['regex']:
+            for regx in reg['regex']:
                 if re.match(regx, s):
                     match = True
             res += match
             
         if res / batch.shape[0] > threshold:
-            for t in reg[1]: tags.add(t)
+            for t in reg['tags']: tags.add(t)
     return set_to_csv(tags)
         
 #TODO make it so each entry is a dict with 3 keys, not tuple
 _regulars = [
-    ({'header': ['id'], 
-        'regex': [      
-        ]
-    }, 'id'),
-    
-    ({'header': ['dt', 'date'],
-        'regex': [
+    {
+        'header' : ['id'], 
+        'tags'   : ['id'],
+        'regex'  : []
+    },
+    {    
+        'header' : ['dt', 'date'],
+        'tags'   : ['date, temporal'],
+        'regex'  : [
             '^\d{4}-\d{2}-\d{2}([\+|\-]\d{2}:\d{2})?', 
             '^(0?[1-9]|1[0-2])[\/](0?[1-9]|[12]\d|3[01])[\/]\d{1,4}$',
             '^(0?[1-9]|1[0-2])[-](0?[1-9]|[12]\d|3[01])[-]\d{1,4}$',
@@ -99,84 +98,102 @@ _regulars = [
             '^(0[1-9]|[12]\d|3[01])[\/](0[1-9]|1[0-2])[\/]\d{1,4}$',
             '^(0[1-9]|[12]\d|3[01])[-](0[1-9]|1[0-2])[-]\d{1,4}$' 
         ]
-    }, 'date, temporal'),
-    
-    ({'header': ['datetime'],
-        'regex': [
+    },
+    {
+        'header' : ['datetime'],
+        'tags'   : ['datetime, temporal'],
+        'regex'  : [
             '^(?=\d)(?:(?:(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})|(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))|(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2}))($|\ (?=\d)))?(((0?[1-9]|1[012])(:[0-5]\d){0,2}(\ [AP]M))|([01]\d|2[0-3])(:[0-5]\d){1,2})?$',
         ]
-    }, 'datetime, temporal'),
-    
-    ({'header': ['time'],
-        'regex': [
+    },
+    {
+        'header' : ['time'],
+        'tags'   : ['time, temporal'],
+        'regex'  : [
             '^(0?[1-9]|1[0-2]):[0-5][0-9]$',
             '^((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))$',
             '^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$',
             '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$',
             '^(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)$'
         ]
-    }, 'time, temporal'),
+    }, 
     
-    ({'header': ['description'],
-        'regex': []
-    }, 'description'),
+    {
+        'header' : ['description'],
+        'tags'   : ['description'],
+        'regex'  : []
+    },
     
-    ({'header': ['gender'],
-        'regex': []
-    }, 'gender, personal'),
+    {
+        'header' : ['gender'],
+        'tags'   : ['gender, personal'],
+        'regex'  : []
+    },
     
     
-    ({'header': ['race'],
-        'regex': []
-    }, 'race, personal'),
+    {
+        'header' : ['race'],
+        'tags'   : ['race', 'personal'],
+        'regex'  : []
+    },
     
-    ({'header': ['diag', 'diagnosis'],
-        'regex': [
+    {
+        'header' : ['diag', 'diagnosis'],
+        'tags'   : ['diagnosis, healthcare'],
+        'regex'  : [
             '^(?i)[a-z]\\d{2}(\\.?[a-z0-9]{1,4})?$'
         ]
-    }, 'diagnosis, healthcare'),
+    },
+    
+    {
+        'header' : ['height'],
+        'tags'   : ['healthcare, numeric, weight'],
+        'regex'  : []
+    }, 
+    
+    {
+        'header' : ['weight'],
+        'tags'   : ['healthcare, numeric, weight'],
+        'regex'  : []
+    },
+    
+    {
+        'header' : ['country', 'cntry'],
+        'tags'   : ['geo, country'],
+        'regex'  : []
+    },
     
     
-    ({'header': ['height'],
-        'regex': []
-    }, 'healthcare, numeric, weight'),
-    
-    
-    ({'header': ['weight'],
-        'regex': [
-        ]
-    }, 'healthcare, numeric, weight'),
-    
-    
-    ({'header': ['country', 'cntry'],
-        'regex': [
-        ]
-    }, 'geo, country'),
-    
-    
-    ({'header': ['email', 'mail'],
-        'regex': [
+    {
+        'header' : ['email', 'mail'],
+        'tags'   : ['contact, email'],
+        'regex'  : [
             """^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$""",
             "/@/"
         ]
-    }, 'contact, email'),
+    },
     
-    ({'header': ['latitude', 'lat'],
-        'regex': [
+    {
+        'header' : ['latitude', 'lat'],
+        'tags'   : ['geo, float, latitude'],
+        'regex'  : [
             """^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$"""
         ]
-    }, 'geo, float, latitude'),
+    },
        
-    ({'header': ['longitude'],
-        'regex': [
+    {
+        'header' : ['longitude'],
+        'tags'   : ['geo, float, longitude'],
+        'regex'  : [
             """^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$"""
         ]
-    }, 'geo, float, longitude'),
+    }, 
        
-    ({'header': ['phone'],
-        'regex': [
+    {
+        'header' : ['phone'],
+        'tags'   : ['contact, phone-number'],
+        'regex'  : [
             """^\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})$"""
         ]
-    }, 'contact, phone-number')
-    
+    }
 ]
