e254bc71c3e72b22ff823577b265201311258516
Denys Zinoviev <reiisbetterthanasuka@gmail.com>
2023-02-14 23:46:25 +0100

    Implemeted features importances

--- modules/.ipynb_checkpoints/analyzer-checkpoint.py
+++ modules/.ipynb_checkpoints/analyzer-checkpoint.py
@@ -14,15 +14,20 @@ import pandas as pd
 
 characters = string.printable
 
-def stats(x):
-    return np.array([x.min(), x.max(), x.mean(), x.std()])
+def stats(x, keyname):
+    return {
+		keyname+'_min': x.min(),
+		keyname+'_max': x.max(), 
+		keyname+'_mean': x.mean(),
+		keyname+'_std': x.std()
+	}
 
 def to_str(seq):
     return np.array(list(map(str, seq)))
 
 def length(seq):
     res = list(map(len, seq))
-    return stats(np.array(res))
+    return stats(np.array(res), 'length')
 
 # This function takes two arguments:
 #
@@ -35,51 +40,51 @@ def length(seq):
 #   each symbol and then a sum is divided by element's length
 #
 
-def filtered_character(seq, filter_func):
+def filtered_character(seq, filter_func, keyname):
     def len_(s):
         if len(s) == 0: 
             return 1 
         else: 
             return len(s)
     res = list(map(lambda s: sum([filter_func(c) for c in s]) / len_(s), seq))
-    return stats(np.array(res))
+    return stats(np.array(res), keyname)
 
 def numeric(seq):
-    res = filtered_character(seq, lambda c: c.isnumeric())
-    return stats(np.array(res))
+    return filtered_character(seq, lambda c: c.isnumeric(), 'numeric')
 
 def alphabetic(seq):
-    res = filtered_character(seq, lambda c: c.isalpha())
-    return stats(np.array(res))
+    return filtered_character(seq, lambda c: c.isalpha(), 'alphabetic')
 
 def uppercase(seq):
-    res = filtered_character(seq, lambda c: ord(c) >= ord('A') and ord(c) <= ord('Z'))
-    return stats(np.array(res))
+    return filtered_character(seq, lambda c: ord(c) >= ord('A') and ord(c) <= ord('Z'), 'uppercase')
+
 
 def each_character(seq):
-    res = np.array([])
+    res = {}
     for ch in characters:
-        res = np.append(filtered_character(seq, lambda c: c == ch), res)
+        res.update(filtered_character(seq, lambda c: c == ch, ch+'_occurrence'))
     return res
+
     
 def uniques(seq: np.ndarray) -> np.ndarray:
-    return np.array([len(np.unique(seq)) / len(seq)])
+    return {"uniqueness": len(np.unique(seq)) / len(seq)}
     
 def each_character_on_prefix(seq, pref_len=10):
-    res = np.array([])
+    res = {}
     for i in range(pref_len):
         for ch in characters:
             occs = 0
             for s in seq:
                 if len(s) > i and s[i] == ch:
                     occs += 1
-            res = np.append(res, np.array([occs / seq.shape[0]]))
+            key = f"{ch}_occurence_on_position_{i}"
+            res[key] = occs / seq.shape[0]
     return res
         
 def empties(seq):
-    return np.array([(sum(list(map(lambda s: len(s) == 0, seq))) / len(seq))])
+    return {"empties": (sum(list(map(lambda s: len(s) == 0, seq))) / len(seq))}
     
-def features(seq):
+def extract_features(seq):
     detectors = [
         length,
         numeric,
@@ -90,8 +95,9 @@ def features(seq):
         each_character_on_prefix,
     ]
     
-    features = np.array([])
+    features = {}
     for detector in detectors:
-        features = np.append(detector(to_str(seq)), features)
-    return features
+        features.update(detector(to_str(seq)))
+	
+    return np.array(list(features.values())), np.array(list(features.keys()))
     
